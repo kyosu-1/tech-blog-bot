18f561ed4b315b8f34272f8751e49ceced622c09
kyosu-1 <sho013039@gmail.com>
2022-03-26 11:10:47 +0900

    webhook urlをパス指定する形に

--- main.py
+++ main.py
@@ -9,20 +9,25 @@ from utils.qiita import get_trend_items, make_text_from_trend_items
 
 
 load_dotenv()
-WEB_HOOK_URL = os.getenv('WEBHOOK_URL')
 
 
-def main():
+def main(webhook_url: str):
     try:
         json_open = open('qiita.json', 'r')
         json_load = json.load(json_open)
         trend_items = get_trend_items(json_load['tag_list'], json_load['tread_get_num'])
         text = make_text_from_trend_items(trend_items)
-        post_slack_by_webhook(WEB_HOOK_URL, text)
+        post_slack_by_webhook(webhook_url, text)
     except Exception as e:
         text = f"[{e}]{traceback.format_exc()}"
-        post_slack_by_webhook(WEB_HOOK_URL, text)
+        post_slack_by_webhook(webhook_url, text)
 
 
 if __name__ == '__main__':
-    main()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--webhook_url', type=str, required=True)
+    args = parser.parse_args()
+
+    main(args.webhook_url)
